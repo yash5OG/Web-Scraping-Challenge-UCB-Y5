6c817c06d21d33b395759884c71bb78f903660f9
Yash5 <yashalphabetagamma@gmail.com>
2021-03-13 23:51:01 -0700

    f3

--- Mission_to_Mars/app.py
+++ Mission_to_Mars/app.py
@@ -11,16 +11,16 @@ mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_app")
 
 @app.route("/")
 def index():
-    return "Hello World"
-#     mars = mongo.db.mars.find_one()
-#     return render_template("index.html", mars=mars)
+    # return "Hello World"
+    mars = mongo.db.mars.find_one()
+    return render_template("index.html", mars=mars)
 
-# @app.route("/scrape")
-# def scrape():
-#     mars = mongo.db.mars
-#     mars_data = scrape_mars.scrape_all()
-#     mars.update({},mars_data, upsert=True)
-#     return redirect("/", code=302)
+@app.route("/scrape")
+def scrape():
+    mars = mongo.db.mars
+    mars_data = scrape_mars.scrape_all()
+    mars.update({},mars_data, upsert=True)
+    return redirect("/", code=302)
 
 
 if __name__ == "__main__":
